a045c0c6e832b2d60b29bb1f26c23157d53b3328
Rui Ribeiro <ruyrybeyro@gmail.com>
2024-03-26 13:58:19 +0000

    first version

---
+++ k8s.sh
@@ -0,0 +1,112 @@
+#!/bin/bash -x
+
+#hostnamectl set-hostname k8s01
+
+# Update and upgrade packages
+sudo dnf update -y
+
+# Install necessary packages
+sudo dnf install -y jq curl tar vim wget firewalld yum-utils ca-certificates gnupg ipset ipvsadm iproute-tc git net-tools bind-utils
+
+# make rsyslog a bit less noisy
+cat <<EOF | sudo tee /etc/rsyslog.d/01-blocklist.conf
+if $msg contains "run-containerd-runc-k8s.io" and $msg contains ".mount: Deactivated successfully." then {
+    stop
+}
+EOF
+sudo systemctl restart rsyslog
+
+# Prerequisites for kubeadm
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --add-port=6443/tcp
+sudo firewall-cmd --permanent --add-port=2379-2380/tcp
+sudo firewall-cmd --permanent --add-port=10250/tcp
+sudo firewall-cmd --permanent --add-port=10251/tcp
+sudo firewall-cmd --permanent --add-port=10252/tcp
+sudo firewall-cmd --permanent --add-port=10255/tcp
+sudo firewall-cmd --permanent --add-port=10257/tcp
+sudo firewall-cmd --permanent --add-port=10259/tcp
+sudo firewall-cmd --permanent --add-port=30000-32767/tcp 
+sudo firewall-cmd --reload
+
+# overlay, br_netfilter and forwarding for k8s
+sudo mkdir -p /etc/modules-load.d/
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+sudo mkdir -p /etc/sysctl.d/
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+sudo sysctl --system
+
+#sudo dracut -f
+
+
+sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
+sudo dnf install -y containerd
+containerd config default | sed 's/SystemdCgroup = false/SystemdCgroup = true/g;s|"/run/containerd/containerd.sock"|"/var/run/containerd/containerd.sock"|g' | sudo tee /etc/containerd/config.toml
+sudo systemctl enable --now containerd
+
+# Install Kubernetes
+LATEST_RELEASE=$(curl -sSL https://dl.k8s.io/release/stable.txt | sed 's/\(\.[0-9]*\)\.[0-9]*/\1/')
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/${LATEST_RELEASE}/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/${LATEST_RELEASE}/rpm/repodata/repomd.xml.key
+EOF
+
+sudo dnf update
+sudo dnf install -y kubectl kubeadm kubelet
+sudo systemctl enable --now kubelet
+
+## master specific stuff
+
+# Install CNI plugins
+DEST_DIR="/opt/cni/bin"
+sudo mkdir -p $DEST_DIR
+LATEST_RELEASE=$(curl -s "https://api.github.com/repos/containernetworking/plugins/releases/latest" | awk -F'"' '/tag_name/{print $4}')
+OS="linux"
+ARCH="amd64"
+URL="https://github.com/containernetworking/plugins/releases/download/$LATEST_RELEASE/cni-plugins-$OS-$ARCH-$LATEST_RELEASE.tgz"
+wget "$URL" -O /tmp/cni-plugins.tgz
+sudo tar -C $DEST_DIR -xzvf /tmp/cni-plugins.tgz
+rm /tmp/cni-plugins.tgz
+
+cat <<EOF | tee kubeadm-config.yaml
+---
+apiVersion: "kubeadm.k8s.io/v1beta3"
+kind: InitConfiguration
+---
+apiVersion: kubelet.config.k8s.io/v1beta1
+kind: KubeletConfiguration
+failSwapOn: false
+featureGates:
+  NodeSwap: true
+memorySwap:
+  swapBehavior: LimitedSwap
+EOF
+
+# Temporary command ignoring warnings till I get a complete setup running with recommended specs
+sudo kubeadm init --ignore-preflight-errors=NumCPU,Mem --config kubeadm-config.yaml
+
+mkdir -p "$HOME"/.kube
+sudo cp -i /etc/kubernetes/admin.conf "$HOME"/.kube/config
+sudo chown "$(id -u)":"$(id -g)" "$HOME"/.kube/config
+
+
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+
+#echo insecure > "$HOME"/.curlrc
+
+kubectl get node -w
