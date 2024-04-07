0dfc0c46de4a2547d017dedf9e667ee6903f8b6b
Rui Ribeiro <ruyrybeyro@gmail.com>
2024-04-07 17:13:26 +0100

    having less error messages

--- k8s.sh
+++ k8s.sh
@@ -164,7 +164,7 @@ EOF4
     sudo kill -HUP 1
 
     # fixing annoying RH 9 issue giving a lot of console error messages
-    chmod a+x /etc/rc.d/rc.local
+    sudo chmod a+x /etc/rc.d/rc.local 2> /dev/null
 }
 
 InterfaceWithcontainerd()
