4e24378a5b154b3fe5bff90c064355f282e72ebd
Rui Ribeiro <ruyrybeyro@gmail.com>
2024-04-09 13:56:04 +0100

    linting

--- k8s.sh
+++ k8s.sh
@@ -252,10 +252,10 @@ CNI()
     CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
     GOOS=$(go env GOOS)
     GOARCH=$(go env GOARCH)
-    curl -L --remote-name-all https://github.com/cilium/cilium-cli/releases/download/${CILIUM_CLI_VERSION}/cilium-${GOOS}-${GOARCH}.tar.gz{,.sha256sum}
-sha256sum --check cilium-${GOOS}-${GOARCH}.tar.gz.sha256sum
-    sudo tar -C /usr/local/bin -xzvf cilium-${GOOS}-${GOARCH}.tar.gz
-    rm cilium-${GOOS}-${GOARCH}.tar.gz{,.sha256sum}
+    curl -L --remote-name-all https://github.com/cilium/cilium-cli/releases/download/"${CILIUM_CLI_VERSION}/cilium-${GOOS}-${GOARCH}".tar.gz{,.sha256sum}
+sha256sum --check cilium-"${GOOS}-${GOARCH}".tar.gz.sha256sum
+    sudo tar -C /usr/local/bin -xzvf cilium-"${GOOS}-${GOARCH}".tar.gz
+    rm cilium-"${GOOS}-${GOARCH}".tar.gz{,.sha256sum}
 
 
     helm repo add cilium https://helm.cilium.io/
