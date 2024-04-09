198ecdf3edeb273c413379555308f1f5ab0b76b8
Rui Ribeiro <ruyrybeyro@gmail.com>
2024-04-09 22:10:26 +0100

    silent curl

--- k8s.sh
+++ k8s.sh
@@ -254,7 +254,7 @@ CNI()
     CILIUM_CLI_VERSION=$(curl -s https://raw.githubusercontent.com/cilium/cilium-cli/main/stable.txt)
     GOOS=$(go env GOOS)
     GOARCH=$(go env GOARCH)
-    curl -L --remote-name-all https://github.com/cilium/cilium-cli/releases/download/"${CILIUM_CLI_VERSION}/cilium-${GOOS}-${GOARCH}".tar.gz
+    curl -sL --remote-name-all https://github.com/cilium/cilium-cli/releases/download/"${CILIUM_CLI_VERSION}/cilium-${GOOS}-${GOARCH}".tar.gz
     sudo tar -C /usr/local/bin -xzvf cilium-"${GOOS}-${GOARCH}".tar.gz
     rm cilium-"${GOOS}-${GOARCH}".tar.gz
 
